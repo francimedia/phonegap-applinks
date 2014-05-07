d32f0e10546dc5f8e097d73b1bd2cc46528d449d
Wilkins <wilkinsf@gmail.com>
2014-05-06 22:10:17 -0400

    Adding cordova plugin "inappbrowser"

--- angularjs/platforms/ios/www/cordova_plugins.js
+++ angularjs/platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.inappbrowser": "0.4.0"
+}
 // BOTTOM OF METADATA
 });
